d711a1731b1386ffe801fffed85ddbbd34a7f5a9
OzyBehindScreen <adriantech42@gmail.com>
2026-01-19 23:19:50 +0530

    Added additional modes for streaming

--- core_components/streaming/stream_01.py
+++ core_components/streaming/stream_01.py
@@ -21,21 +21,55 @@ agent = create_agent(
 
 # 4. Execute the Agent using 'stream' mode.
 # Streaming allows you to see the intermediate steps (thoughts/tool calls) as they happen.
-for chunk in agent.stream(  
+# for chunk in agent.stream(  
+#     {"messages": [{"role": "user", "content": "What is the weather in SF?"}]},
+#     stream_mode="updates", # This mode yields updates whenever a node in the agent's graph finishes.
+# ):
+#     # 'chunk' represents a snapshot of the current state of the agent's execution.
+#     for step, data in chunk.items():
+#         print(f"\n--- step: {step} ---")
+        
+#         # Access the last message generated in this specific step.
+#         # This will usually be either a "Tool Call" (AI) or a "Tool Result" (System).
+#         last_msg = data['messages'][-1]
+        
+#         # .content_blocks contains the structured data (like the tool name and arguments).
+#         if hasattr(last_msg, 'content_blocks'):
+#             print(f"content: {last_msg.content_blocks}")
+#         else:
+#             # Fallback for simple string responses (the final answer).
+#             print(f"content: {last_msg.content}")
+
+# 5. Execute the Agent using 'stream message mode'
+# Streaming mode messages allows you to see the output of the agent streaming tool calls and the final response.
+
+for token, metadata in agent.stream(  
     {"messages": [{"role": "user", "content": "What is the weather in SF?"}]},
-    stream_mode="updates", # This mode yields updates whenever a node in the agent's graph finishes.
+    stream_mode="messages", # Streams tuples of (token, metadata) from any graph nodes where an LLM is invoked.
 ):
-    # 'chunk' represents a snapshot of the current state of the agent's execution.
-    for step, data in chunk.items():
-        print(f"\n--- step: {step} ---")
-        
-        # Access the last message generated in this specific step.
-        # This will usually be either a "Tool Call" (AI) or a "Tool Result" (System).
-        last_msg = data['messages'][-1]
-        
-        # .content_blocks contains the structured data (like the tool name and arguments).
-        if hasattr(last_msg, 'content_blocks'):
-            print(f"content: {last_msg.content_blocks}")
-        else:
-            # Fallback for simple string responses (the final answer).
-            print(f"content: {last_msg.content}")
+    print(f"node: {metadata['langgraph_node']}")
+    print(f"content: {token}")
+    print("\n")
+
+
+# 6. Customer updates
+
+from langgraph.config import get_stream_writer
+
+def get_star_sign(number_of_stars: str) -> str:
+    """Given number of star tell the potential star sign"""
+    writer = get_stream_writer()
+    # This data is what appears in 'stream_mode="custom"'
+    writer(f"Looking up data for stars") 
+    writer(f"Acquired data for {number_of_stars}")
+    return f"It's Aquarius"
+
+agent_custom = create_agent(llm, tools=[get_star_sign])
+
+print("\n--- RUNNING SECTION 6 ---")
+# Prompt must be specific enough to trigger the tool
+for chunk in agent_custom.stream(
+    {"messages": [{"role": "user", "content": "What is the star sign for 12 stars?"}]},
+    stream_mode="custom"
+):
+    print(f"Custom Chunk: {chunk}")
